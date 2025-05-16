8b24a7976a8fc62ab06e34881b245f0b8f135b7f
Victoria  <96148132+VictoriaSouzaS@users.noreply.github.com>
2025-05-16 14:39:49 -0300

    Fix core

--- src/servico_pedidos/core.clj
+++ src/servico_pedidos/core.clj
@@ -1,16 +1,19 @@
 (ns servico-pedidos.core
   (:require [jackdaw.client :as kafka]
-            [jackdaw.serdes.json :as json]
-            [config.config :refer [db-config kafka-config]]))
+            [jackdaw.serdes.json :as json-serde]
+            [config.config :refer [kafka-config]]))
 
 (defn criar-produtor []
-  {"bootstrap.servers" (:bootstrap-servers (:kafka config))})
+  {"bootstrap.servers" (:bootstrap-servers kafka-config)
+   "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
+   "value.serializer" "org.apache.kafka.common.serialization.StringSerializer"})
 
 (defn criar-topico []
-  {:topic-name (:pedido-topic (:kafka config)) ; Usando a configuração do tópico
-   :key-serde (json/serde)
-   :value-serde (json/serde)})
+  {:topic-name (:pedido-topic kafka-config)
+   :key-serde (json-serde/serde)
+   :value-serde (json-serde/serde)})
 
+;;; só pra um teste 
 (defn gerar-pedido []
   {:id (str (java.util.UUID/randomUUID))
    :cliente-id "123"
@@ -18,7 +21,7 @@
    :itens [{:produto "Livro" :quantidade 1}]})
 
 (defn -main [& _]
-  (let [produtor-config (criar-produtor)   ; Renomeado para evitar conflito com 'config'
+  (let [produtor-config (criar-produtor)
         topico (criar-topico)]
     (with-open [produtor (kafka/producer produtor-config)]
       (println "🚀 Enviando pedido...")
@@ -29,3 +32,10 @@
         (kafka/send! produtor topico registro)
         (.flush produtor)
         (println "✅ Pedido enviado com sucesso:" pedido)))))
+
+
+;;Configura e cria um produtor Kafka.
+;; ;; Define um tópico Kafka com as serdes para serializar as mensagens em JSON.
+;;; Gera um objeto “pedido” de exemplo (pedido de compra).
+;;; Envia o pedido para o tópico Kafka usando o produtor.
+;;;Fecha o produtor com segurança após o envio.
